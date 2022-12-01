b2c904cab7c1a1791a4a67183506fed461ab7d8d
Annie Leclerc <14283027+anniecarbo@users.noreply.github.com>
2022-12-01 15:55:06 -0500

    Add custom sized gris

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-// Grid 16x16
+//Ici la fenetre pour que le joueur choissise la taille de la grille
+let playerPrompt = prompt("Enter number of squares per side");
+let gridSize = playerPrompt * playerPrompt
 
-let cell = document.createElement("div");
-cell.classList.add("square");
 
-let divClone = cell.cloneNode(true);
 
-function appendNCopies(n, original, appendTo) {
-    for(var i = 0; i < n; i++) {
-        var clone = original.cloneNode(true);
-        appendTo.appendChild(clone);
-    }
-}
+    
+//Ici pour que la grille soit formee selon le chiffre du joueur
+const container = document.getElementById("grid");
 
-appendNCopies(256, cell, document.getElementById("grid"));
+function makeRows(rows, cols) {
+  grid.style.setProperty('--grid-rows', rows);
+  grid.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    grid.appendChild(cell).className = "square";
+  };
+};
 
+makeRows(playerPrompt, playerPrompt);
+
+
+// Ici la creation de la grille par defaut
 
 // Black hover 
 let elem = document.getElementsByClassName('square');
@@ -26,7 +33,5 @@ elemArray.forEach((item) => {
 })
 
 
-//Ici la fenetre pour que le joueur choissise la taille de la grille
-function popup() {
-let playerPrompt = prompt("Enter number of squares per side");
-}
+
+
